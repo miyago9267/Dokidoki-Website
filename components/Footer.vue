85316d5b0782fc16cea6fa268e54599496d27cc2
miyago9267 <miyagomizugen@gmail.com>
2024-09-09 17:32:19 +0800

    feat: create footer

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="h-15 w-full bg-gray-5">
+        <div class="flex h-10 footer justify-center">
+            <p class="text-sm">
+                2024©Copyright from <a :href="link" target="_blank" class="underline text-inherit">幹幹貓工作室</a>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const link = ''
+</script>
