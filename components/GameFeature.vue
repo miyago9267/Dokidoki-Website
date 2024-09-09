8e7f6976bf4e5f8078b273f4cecea16d23a852ae
miyago9267 <miyagomizugen@gmail.com>
2024-09-09 17:31:42 +0800

    fix: solve text size out of frame

--- components/GameFeature.vue
+++ components/GameFeature.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="w-full h-full">
-        <div class="text-[40px] font-black w-[90%] ml-[8%] my-[2%] text-[#C43E87]">遊戲特色</div>
-        <div class="w-full mt-[2%] flex grow">
+        <div class="text-[50px] font-black w-[90%] ml-[8%] my-[2%] text-[#C43E87]">遊戲特色</div>
+        <div class="w-full mt-[2%] mb-[10%] flex grow">
             <div
-                class="flex flex-col bg-no-repeat w-full items-center h-[480px] justify-center"
+                class="flex flex-col pt-[8%] pb-[15%] bg-no-repeat w-full items-center"
                 style="background-image: url('/images/bg2.png'); background-position: 50% 10%; background-size: 130%"
             >
-                <p class="text-[2vw] text-[#C43E87] font-black font-['微軟正黑體'] w-[60%] ">
+                <p
+                    class="mt-[6%] text-[2vw] text-[#C43E87] p-7 font-black w-[60%]"
+                    style="background-color: #fcf0f2;"
+                >
                     <span class="fond-black"
                         style="text-shadow: -0.3px 0 #C43E87, 0 0.3px #C43E87, 0.3px 0 #C43E87, 0 -0.3px #C43E87;"
                     >《心動不已：告白日記》</span>是一款第二人稱扮演的視覺小說遊戲，
@@ -17,8 +20,9 @@
                     <br>
                     你將可以與她們交談、約會，並選出你認為最有可能的那位女性。
                 </p>
-                <p class="justify-center text-[#C43E87] text-[2vw] mt-[4%] md:mt-[4.5%] font-black text-shadow-[#C43E87]"
-                    style="text-shadow: -0.3px 0 #C43E87, 0 0.3px #C43E87, 0.3px 0 #C43E87, 0 -0.3px #C43E87;">
+                <p
+                    class="justify-center text-[#C43E87] text-[2vw] p-7 mt-[4%] md:mt-[5%] font-black text-shadow-[#C43E87]"
+                    style="text-shadow: -0.3px 0 #C43E87, 0 0.3px #C43E87, 0.3px 0 #C43E87, 0 -0.3px #C43E87;background-color: #fcf0f2;">
                     本遊戲由幹幹貓工作室進行開發，2024年絕讚籌備中！</p>
             </div>
         </div>
