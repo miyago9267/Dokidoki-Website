8a843f8524367dfbb645b97f7576c6da636d35ae
miyago9267 <miyagomizugen@gmail.com>
2024-11-28 14:23:24 +0800

    feat: add loading page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,32 @@
 <template>
-	<div class="min-h-screen bg-[#F6F1F1]">
-		<el-container class="h-full">
-			<el-header class="h-[130px]! mx-[5%]! pt-[20px]!">
-				<Header />
-    		</el-header>
-			<el-container class="main-continaer">
-				<NuxtPage />
+	<div class="relative">
+		<Loading v-if="!isAppLoaded"/>
+		<div class="min-h-screen bg-[#F6F1F1] relative" v-if="isAppLoaded">
+			<el-container class="h-full relative">
+				<el-header class="h-[90px]! mx-[5%]! pt-[20px]! z-[1000]">
+					<Header />
+				</el-header>
+				<el-container class="main-continaer">
+					<NuxtPage />
+				</el-container>
+				<el-footer class="w-screen z-[1000]">
+					<Footer />
+				</el-footer>
 			</el-container>
-			<el-footer class="w-screen">
-				<Footer />
-			</el-footer>
-		</el-container>
+		</div>
 	</div>
 </template>
 
 <script setup>
+import { useState } from '#app'
+
+const isAppLoaded = useState('isAppLoaded', () => false)
+
+if (!isAppLoaded.value) {
+  setTimeout(() => {
+    isAppLoaded.value = true
+  }, 2000)
+}
 </script>
 
 <style scope>
