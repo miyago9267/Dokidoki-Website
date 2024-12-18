9b9cb09ddf1847a987377a127e1f597a9c9c0e4c
miyago9267 <miyagomizugen@gmail.com>
2024-12-18 14:51:03 +0800

    style: make phone size button

--- components/Character/Clothe.vue
+++ components/Character/Clothe.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full flex flex-row justify-around">
         <div v-for="(clothe) in clothesEntries" @click="changeClothes(clothe[1])"
-        class="flex items-center justify-center w-1/4 h-12 bg-[#e17495] hover:bg-[#c75d7e] rounded-2xl text-2xl font-bold  text-center py-2 px-4">
+        class="flex items-center justify-center md:w-1/4 md:h-12 w-1/5 h-8  bg-[#e17495] hover:bg-[#c75d7e] rounded-2xl text-2xl font-bold  text-center py-2 px-4">
             {{ clothe[0] }}
         </div>
     </div>
